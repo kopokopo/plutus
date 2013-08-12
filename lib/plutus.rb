74711f10457ca2a20a05cb6b1c9505543daff1d4
Dennis Ondeng <dennis@kopokopo.com>
2013-08-12 20:14:20 +0300

    Remove isolated namespace delcaration

--- lib/plutus.rb
+++ lib/plutus.rb
@@ -2,6 +2,9 @@
 require "rails"
 module Plutus
   class Engine < Rails::Engine
-    isolate_namespace Plutus
+    #isolate_namespace Plutus
+    #isolate_namespace Plutus if defined? isolate_namespace  #dondeng rails 3.0 modification
+    #ActionController::Base.send :include, Plutus
+    engine_name :plutus
   end
 end
