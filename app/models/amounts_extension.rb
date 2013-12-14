a7e76a72645b56f81f26716fcad2de901efd6445
Dennis Ondeng <dennis@kopokopo.com>
2013-12-14 22:00:02 +0300

    Switch to SQL sum vs iterating to calculating balances

--- app/models/amounts_extension.rb
+++ app/models/amounts_extension.rb
@@ -3,6 +3,8 @@ module AmountsExtension
   # Returns a sum of the referenced Amount objects.
   def balance
     balance = BigDecimal.new('0')
+
+=begin
     find_each do |amount_record|
       if amount_record.amount
         balance += amount_record.amount
@@ -10,6 +12,8 @@ module AmountsExtension
         balance = nil
       end
     end
+=end
+    balance += sum(:amount)
     return balance
   end
 
@@ -26,6 +30,7 @@ module AmountsExtension
   #
   def current_balance
     balance = BigDecimal.new('0')
+=begin
     in_current_quarter.find_each do |amount_record|
       if amount_record.amount
         balance += amount_record.amount
@@ -33,6 +38,14 @@ module AmountsExtension
         balance = nil
       end
     end
+=end
+    balance += in_current_quarter.sum(:amount)
+    return balance
+  end
+
+  def test_balance
+    balance = BigDecimal.new('0')
+    balance += in_current_quarter.sum(:amount)
     return balance
   end
 
@@ -40,6 +53,7 @@ module AmountsExtension
   def balance_from_date(query_date)
     balance = BigDecimal.new('0')
     return balance if query_date.nil?
+=begin
     newer_than(query_date).find_each do |amount_record|
       if amount_record.amount
         balance += amount_record.amount
@@ -47,6 +61,8 @@ module AmountsExtension
         balance = nil
       end
     end
+=end
+    balance += newer_than(query_date).sum(:amount)
     return balance
   end
 
@@ -54,6 +70,8 @@ module AmountsExtension
     balance = BigDecimal.new('0')
     return balance if query_time.nil?
     quarter = "#{query_time.year}-#{((query_time.month - 1) / 3) + 1}"
+
+=begin
     in_quarter(quarter).older_than(query_time).find_each do |amount_record|
       if amount_record.amount
         balance += amount_record.amount
@@ -61,6 +79,10 @@ module AmountsExtension
         balance = nil
       end
     end
+=end
+
+    balance += in_quarter(quarter).older_than(query_time).sum(:amount)
+
     return balance
   end
 end
