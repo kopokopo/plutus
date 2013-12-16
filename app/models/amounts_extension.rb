40934f65bf7727bc66a509a60552e86d95eeeecb
Dennis Ondeng <dennis@kopokopo.com>
2013-12-16 12:15:52 +0300

    clean up previously commented code

--- app/models/amounts_extension.rb
+++ app/models/amounts_extension.rb
@@ -3,16 +3,6 @@ module AmountsExtension
   # Returns a sum of the referenced Amount objects.
   def balance
     balance = BigDecimal.new('0')
-
-=begin
-    find_each do |amount_record|
-      if amount_record.amount
-        balance += amount_record.amount
-      else
-        balance = nil
-      end
-    end
-=end
     balance += sum(:amount)
     return balance
   end
@@ -30,15 +20,6 @@ module AmountsExtension
   #
   def current_balance
     balance = BigDecimal.new('0')
-=begin
-    in_current_quarter.find_each do |amount_record|
-      if amount_record.amount
-        balance += amount_record.amount
-      else
-        balance = nil
-      end
-    end
-=end
     balance += in_current_quarter.sum(:amount)
     return balance
   end
@@ -53,15 +34,6 @@ module AmountsExtension
   def balance_from_date(query_date)
     balance = BigDecimal.new('0')
     return balance if query_date.nil?
-=begin
-    newer_than(query_date).find_each do |amount_record|
-      if amount_record.amount
-        balance += amount_record.amount
-      else
-        balance = nil
-      end
-    end
-=end
     balance += newer_than(query_date).sum(:amount)
     return balance
   end
@@ -70,19 +42,7 @@ module AmountsExtension
     balance = BigDecimal.new('0')
     return balance if query_time.nil?
     quarter = "#{query_time.year}-#{((query_time.month - 1) / 3) + 1}"
-
-=begin
-    in_quarter(quarter).older_than(query_time).find_each do |amount_record|
-      if amount_record.amount
-        balance += amount_record.amount
-      else
-        balance = nil
-      end
-    end
-=end
-
     balance += in_quarter(quarter).older_than(query_time).sum(:amount)
-
     return balance
   end
 end
